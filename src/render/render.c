9f6aefc7400b4f00bce3906da11717e6fbe7e26f
kneeble <kneeble@luc.edu>
2026-04-28 09:51:43 -0500

    Refactor rendering logic to improve clarity and performance; replace string drawing functions with inline logic for score and joke display

--- src/render/render.c
+++ src/render/render.c
@@ -6,8 +6,9 @@ extern void drawPixel(int x, int y, int color);
 
 #define LOGO_X ((SCREEN_WIDTH - LOGO_WIDTH) / 2)
 #define LOGO_Y ((SCREEN_HEIGHT - LOGO_HEIGHT) / 2)
+#define COLOR_TEXT 0x000000
 
-void int_to_str(int num, char *str) {
+static void int_to_str(int num, char *str) {
     int i = 0;
 
     if (num == 0) {
@@ -17,8 +18,6 @@ void int_to_str(int num, char *str) {
     }
 
     int temp = num;
-
-    // count digits
     while (temp > 0) {
         temp /= 10;
         i++;
@@ -26,63 +25,22 @@ void int_to_str(int num, char *str) {
 
     str[i] = '\0';
 
-    // fill backwards
     while (num > 0) {
         str[--i] = '0' + (num % 10);
         num /= 10;
     }
 }
 
-void draw_char(int x, int y, char c, int color) {
-    for (int row = 0; row < 8; row++) {
-        for (int col = 0; col < 8; col++) {
-            if (font[(int)c][row] & (1 << (7 - col))) {
-                drawPixel(x + col, y + row, color);
-            }
-        }
-    }
-}
-
-void draw_string(int x, int y, const char *str, int color) {
-    int i = 0;
-    while (str[i]) {
-        draw_char(x + i * 8, y, str[i], color);
-        i++;
-    }
+static int str_len(const char *s) {
+    int n = 0;
+    while (s[n]) n++;
+    return n;
 }
 
 void render(GameState *state) {
     int p1_x = PADDLE_X_OFFSET;
     int p2_x = SCREEN_WIDTH - PADDLE_X_OFFSET - PADDLE_WIDTH;
 
-    // Draws game (backgrund, paddles, ball)
-    for (int y = 0; y < SCREEN_HEIGHT; y++) {
-        for (int x = 0; x < SCREEN_WIDTH; x++) {
-            int color = COLOR_BG;
-
-            if (x >= LOGO_X && x < LOGO_X + LOGO_WIDTH &&
-                y >= LOGO_Y && y < LOGO_Y + LOGO_HEIGHT) {
-                int pixel = luc_logo[(y - LOGO_Y) * LOGO_WIDTH + (x - LOGO_X)];
-                if (pixel != LOGO_TRANSPARENT) {
-                    color = pixel;
-                }
-            }
-
-            if (x >= p1_x && x < p1_x + PADDLE_WIDTH &&
-                y >= state->paddle1_y && y < state->paddle1_y + PADDLE_HEIGHT) {
-                color = COLOR_PADDLE1;
-            } else if (x >= p2_x && x < p2_x + PADDLE_WIDTH &&
-                       y >= state->paddle2_y && y < state->paddle2_y + PADDLE_HEIGHT) {
-                color = COLOR_PADDLE2;
-            } else if (x >= state->ball_x && x < state->ball_x + BALL_SIZE &&
-                       y >= state->ball_y && y < state->ball_y + BALL_SIZE) {
-                color = COLOR_BALL;
-            }
-
-            drawPixel(x, y, color);
-        }
-    }
-    // Score string build
     char s1[4];
     char s2[4];
     int_to_str(state->score1, s1);
@@ -90,7 +48,6 @@ void render(GameState *state) {
 
     char score_text[16];
     int i = 0;
-
     for (int j = 0; s1[j]; j++) score_text[i++] = s1[j];
     score_text[i++] = ' ';
     score_text[i++] = ':';
@@ -98,9 +55,63 @@ void render(GameState *state) {
     for (int j = 0; s2[j]; j++) score_text[i++] = s2[j];
     score_text[i] = '\0';
 
-    // Draw score (top center)
-    draw_string(SCREEN_WIDTH / 2 - 40, 10, score_text, 0x000000);
+    int score_len = str_len(score_text);
+    int joke_len = str_len(state->current_joke);
 
-    // Draw joke (bottom left)
-    draw_string(20, SCREEN_HEIGHT - 30, state->current_joke, 0x000000);
+    int score_x = SCREEN_WIDTH / 2 - score_len * 4;
+    int score_y = 10;
+    int joke_x = 20;
+    int joke_y = SCREEN_HEIGHT - 30;
+
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        int in_logo_row = (y >= LOGO_Y && y < LOGO_Y + LOGO_HEIGHT);
+        int logo_row_base = (y - LOGO_Y) * LOGO_WIDTH;
+
+        int in_p1_row = (y >= state->paddle1_y && y < state->paddle1_y + PADDLE_HEIGHT);
+        int in_p2_row = (y >= state->paddle2_y && y < state->paddle2_y + PADDLE_HEIGHT);
+        int in_ball_row = (y >= state->ball_y && y < state->ball_y + BALL_SIZE);
+
+        int in_score_row = (y >= score_y && y < score_y + 8);
+        int score_row = y - score_y;
+        int in_joke_row = (y >= joke_y && y < joke_y + 8);
+        int joke_row = y - joke_y;
+
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            int color = COLOR_BG;
+
+            if (in_logo_row && x >= LOGO_X && x < LOGO_X + LOGO_WIDTH) {
+                int pixel = luc_logo[logo_row_base + (x - LOGO_X)];
+                if (pixel != LOGO_TRANSPARENT) {
+                    color = pixel;
+                }
+            }
+
+            if (in_p1_row && x >= p1_x && x < p1_x + PADDLE_WIDTH) {
+                color = COLOR_PADDLE1;
+            } else if (in_p2_row && x >= p2_x && x < p2_x + PADDLE_WIDTH) {
+                color = COLOR_PADDLE2;
+            } else if (in_ball_row && x >= state->ball_x && x < state->ball_x + BALL_SIZE) {
+                color = COLOR_BALL;
+            }
+
+            if (in_score_row && x >= score_x && x < score_x + score_len * 8) {
+                int idx = (x - score_x) / 8;
+                int col = (x - score_x) % 8;
+                unsigned char ch = (unsigned char)score_text[idx] & 0x7f;
+                if ((font[ch][score_row] >> (7 - col)) & 1) {
+                    color = COLOR_TEXT;
+                }
+            }
+            if (in_joke_row && x >= joke_x && x < joke_x + joke_len * 8) {
+                int idx = (x - joke_x) / 8;
+                int col = (x - joke_x) % 8;
+                unsigned char ch = (unsigned char)state->current_joke[idx] & 0x7f;
+                if ((font[ch][joke_row] >> (7 - col)) & 1) {
+                    color = COLOR_TEXT;
+                }
+            }
+
+            drawPixel(x, y, color);
+        }
+    }
 }
